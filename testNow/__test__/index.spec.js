f1d3588b75373656f1930d6ec5ce4349a95a1cd1
soye.li <1162739224@qq.com>
2022-05-04 04:36:53 +0800

    feat(jest): 开发自动生成用例工具

--- testNow/__test__/index.spec.js
+++ testNow/__test__/index.spec.js
@@ -1,16 +1,29 @@
-test('测试代码生成', () => {
+const fs = require('fs')
+test('集成测试 测试生成测试代码文件', () => {
+    // 准备环境
+    // 删除测试文件夹
+    console.log('===__dirname',__dirname)
+    if (fs.existsSync(__dirname + '/data/__test__')) {
+        fs.rmdirSync(__dirname + '/data/__test__', {
+            recursive: true // 递归删除里面的： true
+        })
+    }
     const src = new (require('../index'))()
-    const ret = src.getTestSource('fun', 'class')
-    expect(ret).toBe(
-`test('TESTfun', () => {
-    const fun = require('../class')
-    const ret = fun()
-    // expect(ret).toBe('test return')
-})`)
+    src.genJestSource(__dirname + '/data')
 })
-test('测试文件名称', () => {
-    const src = new (require('../index'))()
-    const ret = src.getTestFileName('/abc/class.js')
-    console.log('getTestFileName:' ,ret)
-    expect(ret).toBe('/abc/__test__/class.spec.js')
-})
+// test('测试代码生成', () => {
+//     const src = new (require('../index'))()
+//     const ret = src.getTestSource('fun', 'class')
+//     expect(ret).toBe(
+// `test('TESTfun', () => {
+//     const fun = require('../class')
+//     const ret = fun()
+//     // expect(ret).toBe('test return')
+// })`)
+// })
+// test('测试文件名称', () => {
+//     const src = new (require('../index'))()
+//     const ret = src.getTestFileName('/abc/class.js')
+//     console.log('getTestFileName:' ,ret)
+//     expect(ret).toBe('/abc/__test__/class.spec.js')
+// })
--- testNow/index.js
+++ testNow/index.js
@@ -1,15 +1,56 @@
 // 测试驱动开发: 重点在path包的用法
 const path = require('path');
+const fs = require('fs');
 module.exports = class testNow {
+    /** 
+     * @param {*}
+     */    
+    // resolve('./')执行的默认文件夹
+    genJestSource(sourcePath = path.resolve('./')){
+        const testPath = `${sourcePath}/__test__`
+        if(!fs.existsSync(testPath)) {
+            fs.mkdirSync(testPath)
+        }
+        // 遍历代码文件
+        let list = fs.readdirSync(sourcePath)// 即会读取文件夹也会读取文件 
+        list
+            // 添加完整路径
+            .map(v =>  `${sourcePath}/${v}`)
+            // 过滤文件 statSync留下是文件的 v是一个文件名
+            .filter(v => fs.statSync(v).isFile())
+            // 排除测试代码 要没有的
+            .filter(v => v.indexOf('.spec') === -1)
+            .map(v => this.genTestFile(v))
+    }
+    genTestFile(filename) {
+        const testFileName = this.getTestFileName(filename)
+        console.log('fileam,e', filename)
+        // 判断此文件是否存在
+        if (fs.existsSync(testFileName)) {
+            console.log('已存在',testFileName)
+            return
+        }
+        const mod = require(filename)
+        let source
+        if (typeof mod === 'object') {
+            source = Object.keys(mod)
+                .map(v => this.getTestSource(v, (path.basename(filename)),true))
+                .join('\n')
+        } else if (typeof mod == 'function') {
+            const basename = path.basename(filename)
+            source = this.getTestSource(basename.replace('.js', ''), basename)
+        }
+        fs.writeFileSync(testFileName, source)
+    }
     /**
      * 生成测试文件
      * @param {*} methodName
      * @param {*} classFile
      * @param {*} isClass
      */    
-    getTestSource(methodName, classFile, isClass = false) {
+    getTestSource(methodName, classFile, isClass) {
         return `test('${'TEST'+ methodName}', () => {
-    const ${isClass ? '{' + methodName + '}' : methodName} = require('${'../' + classFile}')
+    const ${isClass ? `{${methodName}}` : methodName } = require('${'../' + classFile}')
     const ret = ${methodName}()
     // expect(ret).toBe('test return')
 })`
